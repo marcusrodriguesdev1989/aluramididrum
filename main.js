5df8d768f4fd199f7201fcbba78a4edb65e68015
marcusrodriguesdev1989 <marcusgers@hotmail.com>
2024-10-01 00:10:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento)
+        
+        if ('') {
+        tecla.classList.add('ativa')
+        }
+    }
+ 
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+}
+
